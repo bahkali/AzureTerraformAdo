477a5bab42882e110c83895c51319386b9e08c26
Kaly Bah <kalimamadou@gmail.com>
2024-11-04 18:09:30 -0500

    Add project files.

---
+++ AzureTerraformAdo/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AzureTerraformAdo.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
